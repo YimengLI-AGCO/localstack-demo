d334c78e66ea178deab0de046f75a407e4d331fe
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-29 16:09:56 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo "[START]Zip the lambda function"
+echo "[START]Zip the lambda functions"
 cd lambda
+rm -f dynamodb_get.py.zip hello1.py.zip
 zip hello1.py.zip hello1.py
+zip dynamodb_get.py.zip dynamodb_get.py
 cd ..
 echo "[DONE]Zip"
 
-echo "[START]Create S3 bucket and mv the zip file"
+pipenv run python scripts/create_dynamodb_table.py
+
+echo "[START]Create S3 bucket and mv the zip files"
 awslocal s3 mb s3://local-test-demo-bucket
 awslocal s3 cp lambda/hello1.py.zip s3://local-test-demo-bucket
+awslocal s3 cp lambda/dynamodb_get.py.zip s3://local-test-demo-bucket
 awslocal s3 ls s3://local-test-demo-bucket
-echo "[DONE]Create S3 bucket and mv the zip file"
+echo "[DONE]Create S3 bucket and mv the zip files"
 
 echo "[START]Clean the old CloudFormation file"
 rm -f ./template.yaml
